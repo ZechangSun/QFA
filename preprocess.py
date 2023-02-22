30a8dbf9e4958843ba3b7d4553f66512517c281a
Anning Gao <anninggao211@gmail.com>
2023-02-22 18:46:08 +0800

    add the rebin function and the comments for each function.

--- preprocess.py
+++ preprocess.py
@@ -16,7 +16,13 @@ CIV2 = 1910
 
 def overlap(sightline, data:DesiMock, id):
     '''
-    here sightline = data.get_sightline(id=id)
+    Deal with the overlapping area between different cameras so that the result will not contain the overlaps.
+    
+    ---
+    
+    ### Parameters
+    `sightline`: the spectra that is waiting to be rebinned. It must have been clipped by `clip()`.
+    `data` and `id`: the original dataset from which the sightline is extracted by `DesiMock().get_sightline()` and the id of this sightline.
     '''
     
     
@@ -101,9 +107,18 @@ def overlap(sightline, data:DesiMock, id):
 #         plt.plot(wavs_new[zero_point:], sigmadown[zero_point:])
 #         plt.axvline(LyALPHA*(1+sightline.z_qso), linestyle='--')
     
-def clip(sightline, unit_default=100, slope=2e-3, plot=False):
+def clip(sightline, unit_default=100, slope=2e-3, ratio=0.5, plot=False):
     '''
-    This function can automatically change the unit according to the slope of the being-clipped area
+    Clip the abnormal points in the spectra.
+    
+    ---
+    
+    ### Parameters
+    `sightline`: the spectra that is waiting to be rebinned. It must have been clipped by `clip()`.
+    `unit_default`: the default length of each bin that is used to conduct sigmaclip.
+    `slope`: the critical value that decides whether a smaller bin will be applied. If the fit slope of current bin exceeds this value, a smaller bin will be used.
+    `ratio`: how small the smaller bin will be compared with the default bin length.
+    `plot`: if true, this function can generate a plot that shows every bin's clipping upper and lower limit as well as the original spectrum that has not been clipped, which can clearly show which points are clipped. Ofter used in jupyter notebook. 
     '''
     
     def line_fit(xdata, ydata):
@@ -118,6 +133,7 @@ def clip(sightline, unit_default=100, slope=2e-3, plot=False):
     flux = sightline.flux
     error = sightline.error
     zero_point = np.where(wavs / (1+sightline.z_qso) >= LyALPHA)[0][0]
+    sightline.points_num = len(wavs)
 
     wavs_new = wavs[0:zero_point]
     flux_new = flux[0:zero_point]
@@ -140,7 +156,7 @@ def clip(sightline, unit_default=100, slope=2e-3, plot=False):
             slope_fit = line_fit(subwavs, subflux)[0]
 
             if np.abs(slope_fit) >= slope:
-                unit = int(unit_default/2)
+                unit = int(unit_default*ratio)
                 end = start + unit
                 if end >= len(wavs):
                     end = len(wavs) - 1
@@ -187,3 +203,100 @@ def clip(sightline, unit_default=100, slope=2e-3, plot=False):
         plt.axvline(LyALPHA*(1+sightline.z_qso), linestyle='--')
         plt.show()
     
+def get_dlnlambda(sightline):
+    '''
+    Generate the step length of restframe grid used in `rebin()`.
+    
+    ----
+
+    ### Attention
+    For the mock data, this function generate the same value for all the spectrum. I am not sure whether this characristic will remain the same for the actual data.
+    '''
+    wavelength = 10**sightline.loglam_cliped
+    pixels_number = sightline.points_num
+    max_wavelength = wavelength[-1]
+    min_wavelength = wavelength[0]
+    dlnlambda = np.log(max_wavelength/min_wavelength)/pixels_number
+    return dlnlambda
+    
+def rebin(sightline, loglam_start, dlnlambda, max_index:int=int(1e6)):
+    '''
+    Rebin to the same restframe grid.
+    
+    --------
+
+    ### Parameters:
+    `sightline`: the spectra that is waiting to be rebinned. It must have been clipped by `clip()`.
+    `loglam_start`: the start point of this restframe grid. Usually it is the start RESTFRAME wavelength of the spectra whose redshift is the largest.
+    `dlnlambda`: the step length of this restframe grid. It can be derived with `get_dlnlambda()`.
+    `max_index`: because different spectra has different range of wavelength in restframe, so it is necessary to make the restframe grid large enough to contain all of these spectrum. This parameter is the size of this grid, which is usually very big. You can change the default value if you think it is too big.
+    '''
+    def get_between(array, max, min):
+        if max >= min:
+            if max >= np.min(array) and min <= np.max(array):
+                return np.intersect1d(np.where(array>=min)[0], np.where(array<=max)[0])
+            else:
+                raise ValueError('min~max out of range')
+        else:
+            raise ValueError('max < min, will return nothing')
+    
+    wavelength = 10**sightline.loglam_cliped / (1+sightline.z_qso)
+    flux = sightline.flux_cliped
+    error = sightline.error_cliped
+    
+    max_wavelength = wavelength[-1]
+    min_wavelength = wavelength[0]
+    new_wavelength_total = 10**loglam_start * np.exp(dlnlambda * np.arange(max_index))
+    indices = get_between(new_wavelength_total, max_wavelength, min_wavelength)
+    new_wavelength = new_wavelength_total[indices]
+    
+    # 以下抄了学长的代码hhh
+    npix = len(wavelength)
+    wvh = (wavelength + np.roll(wavelength, -1)) / 2.
+    wvh[npix - 1] = wavelength[npix - 1] + \
+                    (wavelength[npix - 1] - wavelength[npix - 2]) / 2.
+    dwv = wvh - np.roll(wvh, 1)
+    dwv[0] = 2 * (wvh[0] - wavelength[0])
+    med_dwv = np.median(dwv)
+    
+    cumsum = np.cumsum(flux * dwv)
+    cumvar = np.cumsum(error * dwv, dtype=np.float64)
+    
+    fcum = interp1d(wvh, cumsum,bounds_error=False)
+    fvar = interp1d(wvh, cumvar,bounds_error=False)
+    
+    nnew = len(new_wavelength)
+    nwvh = (new_wavelength + np.roll(new_wavelength, -1)) / 2.
+    nwvh[nnew - 1] = new_wavelength[nnew - 1] + \
+                     (new_wavelength[nnew - 1] - new_wavelength[nnew - 2]) / 2.
+    
+    bwv = np.zeros(nnew + 1)
+    bwv[0] = new_wavelength[0] - (new_wavelength[1] - new_wavelength[0]) / 2.
+    bwv[1:] = nwvh
+    
+    newcum = fcum(bwv)
+    newvar = fvar(bwv)
+    
+    new_fx = (np.roll(newcum, -1) - newcum)[:-1]
+    new_var = (np.roll(newvar, -1) - newvar)[:-1]
+    
+    # Normalize (preserve counts and flambda)
+    new_dwv = bwv - np.roll(bwv, 1)
+    new_fx = new_fx / new_dwv[1:]
+    # Preserve S/N (crudely)
+    med_newdwv = np.median(new_dwv)
+    new_var = new_var / (med_newdwv/med_dwv) / new_dwv[1:]
+    
+    left = 0
+    while np.isnan(new_fx[left])|np.isnan(new_var[left]):
+        left = left+1
+    right = len(new_fx)
+    while np.isnan(new_fx[right-1])|np.isnan(new_var[right-1]):
+        right = right-1
+    
+    test = np.sum((np.isnan(new_fx[left:right]))|(np.isnan(new_var[left:right])))
+    assert test==0, 'Missing value in this spectra!'
+    
+    sightline.loglam_rebin_restframe = np.log10(new_wavelength[left:right])
+    sightline.flux_rebin_restframe = new_fx[left:right]
+    sightline.error_rebin_restframe = new_var[left:right]
